20ed8034f525aa4e3be51383ed03b92c875039ac
jaredshedr <jaredsurfside@gmail.com>
2022-07-07 16:50:27 -0400

    Added server and index.js , as well as script in package json

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+let port = 3000;
+app.listen(port)
+console.log(`listening at localhost : ${port}`)
