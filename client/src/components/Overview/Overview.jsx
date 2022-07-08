a2fe48d5cd16448fc1caf29396c91365a878da1d
Patrick O'Shea <osheap81796@gmail.com>
2022-07-08 14:49:21 -0400

    Added component framework

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ImageGallery from './ImageGallery.jsx';
+import ProductDetails from './ProductDetails.jsx';
+import StyleSelector from './StyleSelector.jsx';
+import PurchaseOptions from './PurchaseOptions.jsx';
 
 function Overview() {
-  return <h2>Overview Widget</h2>;
+  return (
+    <>
+      <h2>Overview Widget</h2>
+      <ImageGallery />
+      <ProductDetails />
+      <StyleSelector />
+      <PurchaseOptions />
+    </>
+  );
 }
 
 export default Overview;
