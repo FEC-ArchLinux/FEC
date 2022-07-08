fd145058a8ef442bb0850d00477abb704979e62d
Patrick O'Shea <osheap81796@gmail.com>
2022-07-08 19:11:43 -0400

    Edited token and added get requests to overview

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import GH_TOKEN from '../../../../token.js';
 import ImageGallery from './ImageGallery.jsx';
 import ProductDetails from './ProductDetails.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import PurchaseOptions from './PurchaseOptions.jsx';
 
-function Overview() {
+function Overview(props) {
+  const [productInfo, setProductInfo] = useState([]);
+  const [productStyleInfo, setProductStyleInfo] = useState([]);
+
+  function getProductInfo() {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${props.productId}`, {
+      headers: {
+        authorization: GH_TOKEN,
+      },
+    })
+      .then((info) => setProductInfo(info.data))
+      .catch((err) => console.error(err));
+  }
+
+  function getProductStyleInfo() {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${props.productId}/styles`, {
+      headers: {
+        authorization: GH_TOKEN,
+      },
+    })
+      .then((info) => setProductStyleInfo(info.data))
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(() => {
+    getProductInfo();
+    getProductStyleInfo();
+  }, []);
+
   return (
     <>
       <h2>Overview Widget</h2>
-      <ImageGallery />
+      <ImageGallery styles={productStyleInfo.results} />
       <ProductDetails />
       <StyleSelector />
       <PurchaseOptions />
---
+++ token.example.js
@@ -0,0 +1,5 @@
+// Copy the contents of this file into a new file called .env.
+
+// The following key-value pairs will be added to process.env
+
+const GH_TOKEN = "";
