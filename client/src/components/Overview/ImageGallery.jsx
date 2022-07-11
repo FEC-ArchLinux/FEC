49a6af35937eed418a3f6168dac3eaefc6d2ae98
Patrick O'Shea <osheap81796@gmail.com>
2022-07-11 19:40:30 -0400

    Added some inline styling

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -35,17 +35,24 @@ function ImageGallery({ styles, activeStyle }, ref) {
 
   let index = -1;
   return (
-    <>
+    <div style={{ width: "400px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <h3>Image Gallery</h3>
+      <div style={{ width: "300px" , marginRight: "10px"}}>
+        {styles && styles[activeStyle].photos.map((photo) => {
+          index++
+          return (
+            <div height="100px" width="100px">
+              <img style={{objectFit: "cover"}} onClick={selectBigPicture} height="100px" width="100px" name={index} src={photo.url} alt="style-img" />
+              <br />
+            </div>
+          );
+        })}
+      </div>
       <h3>Selected Photo</h3>
       <button type="button" id="decrement" onClick={changeBigPicture}>⬅️</button>
-      <img height="500px" src={styles && styles[activeStyle].photos[activeImage].url} alt="enlarged-style" />
+      <img style={{ marginRight: "10px", marginLeft: "10px" }} height="500px" src={styles && styles[activeStyle].photos[activeImage].url} alt="enlarged-style" />
       <button type="button" id="increment" onClick={changeBigPicture}>➡️</button>
-      <h3>Image Gallery</h3>
-      {styles && styles[activeStyle].photos.map((photo) => {
-        index++
-        return <img onClick={selectBigPicture} height="150px" name={index} src={photo.url} alt="style-img" />;
-      })}
-    </>
+    </div>
   );
 }
 
