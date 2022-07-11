7d389c30b77eb047cd9f3c26c1d216773d7d6cfb
jaredshedr <jaredsurfside@gmail.com>
2022-07-11 10:16:21 -0400

    Added the data for single review, Added button to increment page and get another two reviews from API

--- client/src/components/RatingsAndReviews/ReviewList.jsx
+++ client/src/components/RatingsAndReviews/ReviewList.jsx
@@ -1,23 +1,27 @@
+/* eslint-disable prefer-const */
+/* eslint-disable max-len */
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import GH_TOKEN from '../../../../token.js';
-
+import SingleReviewTile from './SingleReviewTile.jsx';
 
 function ReviewList({ productId }) {
   const [reviewInfo, setReviewInfo] = useState([]);
+  let [pageNumber, setPageNumber] = useState(1);
 
   function getReviewInfo() {
     const config = {
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?page=1&count=2&sort="relevant"&product_id=${productId}`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?page=${pageNumber}&count=2&sort="relevant"&product_id=${productId}`,
       headers: {
         Authorization: GH_TOKEN,
       },
     };
     axios(config)
-      .then((response) => setReviewInfo(response.data))
+      .then((response) => setReviewInfo([...reviewInfo].concat(response.data.results)))
       .catch((error) => console.log(error));
   }
 
@@ -25,11 +29,19 @@ function ReviewList({ productId }) {
     getReviewInfo();
   }, []);
 
-  return (
-    <div>
-      <span>{}</span>
-    </div>
-  );
+  function incrementPage() {
+    setPageNumber(pageNumber += 1);
+    getReviewInfo();
+  }
+
+  if (reviewInfo) {
+    return (
+      <div>
+        {reviewInfo.map((review, index) => <SingleReviewTile review={review} key={index} />)}
+        <button onClick={incrementPage} type="button"> More Reviews </button>
+      </div>
+    );
+  }
 }
 
 export default ReviewList;
--- client/src/components/RatingsAndReviews/SingleReviewTile.jsx
+++ client/src/components/RatingsAndReviews/SingleReviewTile.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
-function SingleReviewTile() {
-  return <h2>Review List Tile</h2>;
+function SingleReviewTile({ review }) {
+  return (
+    <div className="review-tile">
+      <span>{review.rating}</span>
+      <span>{review.date}</span>
+      <span>{review.reviewer_name}</span>
+      <span><b>{review.summary}</b></span>
+      <span>{review.body}</span>
+      <span>{review.recommend}</span>
+      <span>{review.response}</span>
+      <span>Rating Helpfulness Yes || No</span>
+    </div>
+  );
 }
 
 export default SingleReviewTile;
