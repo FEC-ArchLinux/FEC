18748956fdca36eb6abb3788466d00198cda25f8
jaredshedr <jaredsurfside@gmail.com>
2022-07-11 11:05:11 -0400

    Refactored Pagination on Reviews and Added Sort Relevance component

--- client/src/components/RatingsAndReviews/ReviewList.jsx
+++ client/src/components/RatingsAndReviews/ReviewList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable prefer-const */
 /* eslint-disable max-len */
 /* eslint-disable react/no-array-index-key */
@@ -7,21 +8,26 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import GH_TOKEN from '../../../../token.js';
 import SingleReviewTile from './SingleReviewTile.jsx';
+import SortRelevance from './SortRelevance.jsx';
 
 function ReviewList({ productId }) {
   const [reviewInfo, setReviewInfo] = useState([]);
-  let [pageNumber, setPageNumber] = useState(1);
+  const [currentTwo, setCurrentTwo] = useState([]);
+  let [pageNumber, setPageNumber] = useState(0);
 
   function getReviewInfo() {
     const config = {
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?page=${pageNumber}&count=2&sort="relevant"&product_id=${productId}`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?page=1&count=500&sort="newest"&product_id=${productId}`,
       headers: {
         Authorization: GH_TOKEN,
       },
     };
     axios(config)
-      .then((response) => setReviewInfo([...reviewInfo].concat(response.data.results)))
+      .then((response) => {
+        setReviewInfo([...reviewInfo].concat(response.data.results));
+        setCurrentTwo(response.data.results.slice(0, 2));
+      })
       .catch((error) => console.log(error));
   }
 
@@ -29,21 +35,20 @@ function ReviewList({ productId }) {
     getReviewInfo();
   }, []);
 
-  function incrementPage() {
-    setPageNumber(pageNumber += 1);
-    getReviewInfo();
+  function incrementReviews() {
+    setPageNumber(pageNumber += 2);
+    setCurrentTwo(currentTwo.concat(reviewInfo.slice(pageNumber, pageNumber + 2)));
   }
 
   if (reviewInfo) {
     return (
       <div>
-        {reviewInfo.map((review, index) => <SingleReviewTile review={review} key={index} />)}
-        <button onClick={incrementPage} type="button"> More Reviews </button>
+        <SortRelevance reviewInfo={reviewInfo} />
+        {currentTwo.map((review, index) => <SingleReviewTile review={review} key={index} />)}
+        {pageNumber >= reviewInfo.length ? null : <button onClick={incrementReviews} type="button"> More Reviews </button>}
       </div>
     );
   }
 }
 
 export default ReviewList;
-
-// reviewInfo.length > 0 ? reviewInfo.results[0].body : null
---
+++ client/src/components/RatingsAndReviews/SortRelevance.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function SortRelevance() {
+  return (
+    <div>hiii dad</div>
+  );
+}
+
+export default SortRelevance;
