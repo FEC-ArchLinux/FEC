afc2443148be8a5e11b7ee6a9fa66fbefe75cd5b
jaredshedr <jaredsurfside@gmail.com>
2022-07-12 15:50:22 -0400

    First Test Passing, adding jest environment jsdom to dependencies

--- client/src/components/RatingsAndReviews/RatingsReviews.test.js
+++ client/src/components/RatingsAndReviews/RatingsReviews.test.js
@@ -1,16 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
-import Enzyme, { shallow } from 'enzyme';
+// import Enzyme, { shallow } from 'enzyme';
 import ReviewList from './ReviewList.jsx';
 
-describe('ReviewList - More Reviews Button', () => {
-  it('should render two reviews initially', () => {
+describe('test', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('ReviewList - Add Reviews Button', () => {
+  it('should render button', () => {
     const reviewList = render(<ReviewList />);
 
-    const button = reviewList.getByText('More Reviews', {
+    const button = reviewList.getByText('Add a Review', {
       selector: 'button',
     });
 
