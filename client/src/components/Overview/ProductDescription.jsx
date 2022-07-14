d33dd4f8192c8e77efc2a2fcaa5b4febc1c9108e
Patrick O'Shea <osheap81796@gmail.com>
2022-07-14 15:02:11 -0400

    Added styling to Features list

--- client/src/components/Overview/ProductDescription.jsx
+++ client/src/components/Overview/ProductDescription.jsx
@@ -14,12 +14,9 @@ function ProductDescription({ productInfo }) {
         <h3>{productInfo.slogan}</h3>
         <p>{productInfo.description}</p>
       </div>
-      <div style={{'border-left': '2px solid' , 'padding-left': '15px'}}>
-        <h4>Features</h4>
-        <ul>
-          {productInfo.features && productInfo.features.map((feature) => <li>{feature.feature}: {feature.value}</li>)}
+        <ul style={{ 'border-left': '2px solid', 'padding-left': '15px', 'list-style-type': 'none' }}>
+          {productInfo.features && productInfo.features.map((feature) => <li>✓ {feature.feature}: {feature.value}</li>)}
         </ul>
-      </div>
     </div>
   );
 }
