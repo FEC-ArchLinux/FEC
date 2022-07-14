9f95be74f578798792ae4173713de47ce742621f
Patrick O'Shea <osheap81796@gmail.com>
2022-07-14 14:45:54 -0400

    Added styling to product description

--- client/src/components/Overview/ProductDescription.jsx
+++ client/src/components/Overview/ProductDescription.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 function ProductDescription({ productInfo }) {
+  const productDescriptionStyle = {
+    display: 'flex',
+    margin: '20px 0px',
+    'justify-content': 'flex-start',
+    gap: '5%',
+  };
+
   return (
-    <div>
-      <h3>Product Description</h3>
-      <h4>{productInfo.slogan}</h4>
-      <p>{productInfo.description}</p>
-      <h4>Features</h4>
-      <ul>
-        {productInfo.features && productInfo.features.map((feature) => <li>{feature.feature}: {feature.value}</li>)}
-      </ul>
+    <div style={productDescriptionStyle}>
+      <div style={{ 'flex-basis': '50%', 'margin-left': '5vw' }}>
+        <h3>{productInfo.slogan}</h3>
+        <p>{productInfo.description}</p>
+      </div>
+      <div style={{'border-left': '2px solid' , 'padding-left': '15px'}}>
+        <h4>Features</h4>
+        <ul>
+          {productInfo.features && productInfo.features.map((feature) => <li>{feature.feature}: {feature.value}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
