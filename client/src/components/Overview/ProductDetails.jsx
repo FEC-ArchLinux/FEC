244f0e8253731e30b3ac1e92569ade33dea9f420
Patrick O'Shea <osheap81796@gmail.com>
2022-07-16 00:31:19 -0400

    Added basic test

--- client/src/components/Overview/ProductDetails.jsx
+++ client/src/components/Overview/ProductDetails.jsx
@@ -55,6 +55,7 @@ function ProductDetails({ productInfo, styles, activeStyle, productId }) {
 
   const ReviewP = styled.p`
     margin: 0;
+    text-decoration: underline;
     :hover {
       cursor: pointer;
     }
@@ -63,7 +64,7 @@ function ProductDetails({ productInfo, styles, activeStyle, productId }) {
   return (
     <div>
       <StarRatings isSelectable="false" starRatedColor="black" numberOfStars={5} starSpacing="2px" starDimension="calc(.3vw + .3vh + 7px)" rating={starRating ? Math.round(starRating * 100) / 100 : 0} />
-      <ReviewP onClick={() => document.getElementById("review-section").scrollIntoView({ behavior: 'smooth' })} style={{ margin: 0 }}>See all {starRating && totalReviews} reviews.</ReviewP>
+      <ReviewP onClick={() => document.getElementById("review-section").scrollIntoView({ behavior: 'smooth' })}>See all {starRating && totalReviews} reviews.</ReviewP>
       <p style={{ 'margin-bottom': 0 }}>{productInfo.category}</p>
       <h2 style={{ 'margin-top': 0, 'margin-bottom': '2px' }}>{productInfo.name}</h2>
       <div style={{ height: "calc(2vh + 1vw)" }}>
